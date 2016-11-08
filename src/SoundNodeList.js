cf2c4bac0d45dd6b6869f958ed236c4a39c3b724
liamscott <liamscott@greenhouse.io>
2016-11-08 13:02:36 -0500

    Loads available sounds from a manifest

--- src/SoundNodeList.js
+++ src/SoundNodeList.js
@@ -1,45 +1,14 @@
 import React from 'react';
 import SoundNode from './SoundNode';
+import sounds from './sound-manifest';
 
 var SoundNodeList = React.createClass({
   render: function() {
-    var sounds = [
-      {
-        name: 'Rain',
-        iconSrc: '/images/icon-rain.png',
-        audioSrc: '/lib/sounds/rain.m3u8'
-      },
-      {
-        name: 'Thunder',
-        iconSrc: '/images/icon-thunder.png',
-        audioSrc: '/lib/sounds/thunderstorm.m3u8'
-      },
-      {
-        name: 'Wind',
-        iconSrc: '/images/icon-wind.png',
-        audioSrc: '/lib/sounds/wind.m3u8'
-      },
-      {
-        name: 'Leaves',
-        iconSrc: '/images/icon-leaves.png',
-        audioSrc: '/lib/sounds/leaves.m3u8'
-      },
-      {
-        name: 'Forest',
-        iconSrc: '/images/icon-forest.png',
-        audioSrc: '/lib/sounds/forest.m3u8'
-      },
-      {
-        name: 'Stream',
-        iconSrc: '/images/icon-stream.png',
-        audioSrc: '/lib/sounds/waterstream.m3u8'
-      }
-    ];
     var soundNodes = sounds.map(function(sound, i){
       return <SoundNode name={sound.name} iconSrc={sound.iconSrc} audioSrc={sound.audioSrc} key={i}/>
     });
     return <ul className="sound-node-list cf">{soundNodes}</ul>;
-  } 
+  }
 });
 
 module.exports = SoundNodeList;
---
+++ src/sound-manifest.js
@@ -0,0 +1,79 @@
+var sounds = [
+  {
+    name: 'Rain',
+    iconSrc: '/images/icon-rain.png',
+    audioSrc: '/lib/sounds/mp3/rain.mp3'
+  },
+  {
+    name: 'Thunder',
+    iconSrc: '/images/icon-thunder.png',
+    audioSrc: '/lib/sounds/mp3/thunderstorm.mp3'
+  },
+  {
+    name: 'Wind',
+    iconSrc: '/images/icon-wind.png',
+    audioSrc: '/lib/sounds/mp3/wind.mp3'
+  },
+  {
+    name: 'Leaves',
+    iconSrc: '/images/icon-leaves.png',
+    audioSrc: '/lib/sounds/mp3/leaves.mp3'
+  },
+  {
+    name: 'Forest',
+    iconSrc: '/images/icon-forest.png',
+    audioSrc: '/lib/sounds/mp3/forest.mp3'
+  },
+  {
+    name: 'Stream',
+    iconSrc: '/images/icon-stream.png',
+    audioSrc: '/lib/sounds/mp3/waterstream.mp3'
+  },
+  {
+    name: 'Seaside',
+    iconSrc: '/images/icon-seaside.png',
+    audioSrc: '/lib/sounds/mp3/seaside.mp3'
+  },
+  {
+    name: 'Water',
+    iconSrc: '/images/icon-water.png',
+    audioSrc: '/lib/sounds/mp3/water.mp3'
+  },
+  {
+    name: 'Fire',
+    iconSrc: '/images/icon-fire.png',
+    audioSrc: '/lib/sounds/mp3/fire.mp3'
+  },
+  {
+    name: 'Summernight',
+    iconSrc: '/images/icon-summernight.png',
+    audioSrc: '/lib/sounds/mp3/summernight.mp3'
+  },
+  {
+    name: 'Coffee',
+    iconSrc: '/images/icon-coffee.png',
+    audioSrc: '/lib/sounds/mp3/coffee.mp3'
+  },
+  {
+    name: 'Train',
+    iconSrc: '/images/icon-train.png',
+    audioSrc: '/lib/sounds/mp3/train.mp3'
+  },
+  {
+    name: 'Fan',
+    iconSrc: '/images/icon-fan.png',
+    audioSrc: '/lib/sounds/mp3/fan.mp3'
+  },
+  {
+    name: 'Whitenoise',
+    iconSrc: '/images/icon-whitenoise.png',
+    audioSrc: '/lib/sounds/mp3/whitenoise.mp3'
+  },
+  {
+    name: 'Pinknoise',
+    iconSrc: '/images/icon-pinknoise.png',
+    audioSrc: '/lib/sounds/mp3/pinknoise.mp3'
+  }
+];
+
+module.exports = sounds;
